9663eb65dd3b9a3939df6bdf2b4d923ec296bb25
chaosir <qq278935403@gmail.com>
2017-12-08 13:53:05 +0800

    welcome

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,9 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+Route::get('/',function(){
+    return view('welcome');
+});
 Route::group(['prefix' => 'admin'],function(){
 
     Route::get('/login','Admin/DefaultController@login');
